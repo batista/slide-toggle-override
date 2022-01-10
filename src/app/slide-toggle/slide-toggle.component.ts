9aa2cd6ca9a03ad7ca2f0b8f16921aaf86284b54
batista <mail@se.rg.io>
2022-01-10 14:49:59 +0100

    2 files modified

--- src/app/slide-toggle/slide-toggle.component.ts
+++ src/app/slide-toggle/slide-toggle.component.ts
@@ -19,32 +19,46 @@ import { MatIcon } from '@angular/material/icon';
   styleUrls: ['slide-toggle.component.scss'],
 })
 export class SlideToggleComponent implements AfterViewInit {
-  @ViewChild(MatSlideToggle) matSlideToggle: MatSlideToggle;
-  @ViewChild('innerButton') matIcon: MatIcon;
-  @ViewChild('innerLabel') innerLabel: ElementRef;
-
+  /** The status of the switch */
   @HostBinding('class.checked')
   checked: boolean = true;
 
+  /** The icon to display when the switch is checked */
   @Input() iconChecked = 'bolt';
+
+  /** The icon to display when the switch is unchecked */
   @Input() iconUnchecked = 'bolt';
 
+  /** The text to display when the switch is checked */
   @Input() innerTextChecked = 'On';
+
+  /** The text to display when the switch is unchecked */
   @Input() innerTextUnchecked = 'Off';
 
+  /** The label of the switch */
   @Input() labelText = 'MyLabel';
 
-  ngAfterViewInit() {
-    this.renderer.appendChild(
-      this.matSlideToggle._thumbEl.nativeElement.children[0],
-      this.matIcon._elementRef.nativeElement
-    );
+  @ViewChild(MatSlideToggle) private matSlideToggle: MatSlideToggle;
+  @ViewChild('innerButton') private matIcon: MatIcon;
+  @ViewChild('innerLabel') private innerLabel: ElementRef;
 
-    this.renderer.appendChild(
-      this.matSlideToggle._thumbBarEl.nativeElement,
-      this.innerLabel.nativeElement
-    );
+  ngAfterViewInit() {
+    this.repositionDomElements();
   }
 
   constructor(private renderer: Renderer2) {}
+
+  /**
+   * Repositions the injected elements inside the MatSlideToggle Component.
+   */
+  private repositionDomElements() {
+    const slideToggleButtonEl =
+      this.matSlideToggle._thumbEl.nativeElement.children[0];
+    const matIconEl = this.matIcon._elementRef.nativeElement;
+    this.renderer.appendChild(slideToggleButtonEl, matIconEl);
+
+    const slideToggleBarEl = this.matSlideToggle._thumbBarEl.nativeElement;
+    const innerLabelSpanEl = this.innerLabel.nativeElement;
+    this.renderer.appendChild(slideToggleBarEl, innerLabelSpanEl);
+  }
 }
