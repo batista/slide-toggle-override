d64146d2b0c728f0177f360826fc05b95b476b59
batista <mail@se.rg.io>
2022-01-10 01:00:40 +0100

    adds label

--- src/app/slide-toggle/slide-toggle.component.ts
+++ src/app/slide-toggle/slide-toggle.component.ts
@@ -1,4 +1,11 @@
-import { Component, AfterViewInit, ViewChild, Renderer2 } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  Renderer2,
+  ElementRef,
+  Input,
+} from '@angular/core';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatIcon } from '@angular/material/icon';
 
@@ -13,16 +20,28 @@ import { MatIcon } from '@angular/material/icon';
 export class SlideToggleComponent implements AfterViewInit {
   @ViewChild(MatSlideToggle) matSlideToggle: MatSlideToggle;
   @ViewChild(MatIcon) matIcon: MatIcon;
+  @ViewChild('innerLabel') innerLabel: ElementRef;
 
   checked: boolean;
 
   readonly ICON_CHECKED = 'task_alt';
   readonly ICON_UNCHECKED = 'highlight_off';
 
+  @Input()
+  TEXT_CHECKED = 'På';
+
+  @Input()
+  TEXT_UNCHECKED = 'Av';
+
   ngAfterViewInit() {
     this.renderer.appendChild(
       this.matSlideToggle._thumbEl.nativeElement.children[0],
       this.matIcon._elementRef.nativeElement
+    );
+
+    this.renderer.appendChild(
+      this.matSlideToggle._thumbBarEl.nativeElement.children[0],
+      this.innerLabel.nativeElement
     );
   }
 
